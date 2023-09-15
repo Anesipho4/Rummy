ea598f0acb08c36d0229a7191c89e2a120e21a51
dcmcd <125665221+dcmcdonogh@users.noreply.github.com>
2023-09-15 19:25:07 +0200

    Update DeckofCards.java
    More format changes, for ease of read.

--- DeckofCards.java
+++ DeckofCards.java
@@ -1,23 +1,30 @@
 import java.util.ArrayList;
 import java.util.List;
-public class DeckofCards{
+
+public class DeckofCards
+{
 	private ArrayList<Cards> deck;
 
-	//constructor:creates deck of standard cards
-	public DeckofCards(){
-		List<String> suits=Cards.getValidSuits();
-		List<String> facenames=Cards.getValidFaceNames();
-		
+	//constructor: creates deck of standard cards
+	public DeckofCards()
+	{
+		List<String> suits = Cards.getValidSuits();
+		List<String> facenames = Cards.getValidFaceNames();
 		deck = new ArrayList<>();
-		for( String suit: suits){
-			for (String facename: facenames){
-				deck.add(new Cards(facename,suit)); //adding cards to deck
+		
+		for (String suit : suits)
+		{
+			
+			for (String facename : facenames)
+			{
+				deck.add(new Cards(facename, suit)); //adding cards to deck
 			}
+			
 		}
 		
 		
 		
-	}
+}
 	
 	
 	
